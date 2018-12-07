191b5c7b8bb2e23c31bb65150f853dd3acee7374
pk2131 <mail@email.ru>
2018-12-07 08:47:45 +0300

    test

--- src/test/java/ru/bykova/cources/spring/spring5/Spring5ApplicationTest.java
+++ src/test/java/ru/bykova/cources/spring/spring5/Spring5ApplicationTest.java
@@ -2,18 +2,39 @@ package ru.bykova.cources.spring.spring5;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
-import org.junit.gen5.api.extension.ExtendWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static lombok.AccessLevel.PRIVATE;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
 
+//@ContextConfiguration(classes = JavaConfig.class)
+//@WithMockUser(authorities="ADMIN")
+//@AutoConfigureMockMvc
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = PRIVATE, makeFinal = true)
 @AllArgsConstructor(onConstructor = @__(@Autowired))
-class Spring5ApplicationTest {
+class Spring5ApplicationTest { //for ru.vlapin.courses.spring.springfundamwntals5.SpringFundamentals5Application
 
     Person person;
 
+    @Test
+    @SneakyThrows
+    @DisplayName("Person injects correctly")
+    void testName() {
+        // given
+        assertThat(person.getName(), is("Уася"));
+        // when
+        System.out.println(person);
+        // then
+    }
 }
