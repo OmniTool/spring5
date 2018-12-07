9775028666325d4880835b6aadf000137649c623
pk2131 <mail@email.ru>
2018-12-07 09:05:57 +0300

    singleton

---
+++ src/main/java/ru/bykova/cources/spring/spring5/singleton/Singleton.java
@@ -0,0 +1,22 @@
+package ru.bykova.cources.spring.spring5.singleton;
+
+// Пример double check locker
+// проблемы возникают при работе нескольких класс-лоадеров => spring
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() {}
+
+    public Singleton getInstance() {
+        if (instance == null)
+        {
+            synchronized (Singleton.class) {
+                if (instance == null){
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
